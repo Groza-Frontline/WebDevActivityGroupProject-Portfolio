9dbd69ecb87f40db12b2f61656b3dc4fb4365d9b
Groza-Frontline <151484854+Groza-Frontline@users.noreply.github.com>
2024-02-05 22:19:11 +0000

    Major changes Login

---
+++ James/resume_james2.js
@@ -0,0 +1,28 @@
+var users = [
+    { username: 'james', password: 'sal'},
+];
+
+function validateForm2() {
+    var usernameInput = document.getElementById('username').value;
+    var passwordInput = document.getElementById('password').value;
+
+    // Check if the entered username and password match any user
+    var isValidUser = false;
+    for (var i = 0; i < users.length; i++) {
+        if (usernameInput === users[i].username && passwordInput === users[i].password) {
+            isValidUser = true;
+            break;
+        }
+    }
+
+    // Display message
+    if (isValidUser) {
+        // Successful login
+        alert('Login successful!');
+        window.location.href = 'resume_james.html';
+    } else {
+        // Failed login
+        var errorMessage = document.getElementById('error-message');
+        errorMessage.innerHTML = 'Invalid username or password. Please try again.';
+    }
+}
